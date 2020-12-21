7e768289c266a81abf38cc4a3ae15e5065434bb4
lihi2910 <lihi2910@gmail.com>
2020-12-21 10:33:05 +0200

    working script2

---
+++ script2/jokes.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const fs = require('fs').promises;
+const path = require('path');
+const oneLinerJoke = require('one-liner-joke');
+const jokeAmount = parseInt(process.env.JOKE_AMOUNT);
+const jokeSubject = process.env.JOKE_SUBJECT;
+const fileName = "jokes";
+let jokeCounter = 0;
+
+async function writeJokes() {
+    while (jokeCounter != jokeAmount) {
+        let joke = oneLinerJoke.getRandomJokeWithTag('family').body + '\n';
+        let data = await fs.readFile(fileName);
+        if (!data.includes(joke)) {
+            await fs.appendFile(fileName, joke);
+            jokeCounter++;
+        }
+    }
+}
+
+writeJokes();
